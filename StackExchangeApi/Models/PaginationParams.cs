599ee44e06e1da88038329628c8ab3442e256932
czerwonogrodzki <czerwonogrodzkipatryk@gmail.com>
2024-04-03 19:31:08 +0200

    Removed commented code

--- StackExchangeApi/Models/PaginationParams.cs
+++ StackExchangeApi/Models/PaginationParams.cs
@@ -4,15 +4,6 @@ namespace StackExchangeApi.Models
 {
 	public class PaginationParams
 	{
-		//private int _pageCount = 1500 / 50;
-
-		//[BindNever]
-		//public int PageCount
-		//{
-		//	get { return _pageCount; }
-		//	set { _pageCount = (value > 50) ? 50 : value; }
-		//}
-
 		public int Page { get; set; } = 1;
 
 		public OrderByEnum? OrderBy { get; set; } = OrderByEnum.Id;
